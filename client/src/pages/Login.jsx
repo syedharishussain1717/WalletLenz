af2276e00ee21bdf006e3a4c700722bb0f4f5793
Syed Haris Hussain Shah <syedharishussainshah17@gmail.com>
2026-09-24 04:25:37 -0500

    installed react router dom

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import { API_URL } from "../config";
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -8,7 +8,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/login", {
+      const response = await fetch(`${API_URL}/api/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
